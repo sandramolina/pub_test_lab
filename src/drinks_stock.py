1ef75aa2f1498312c8b44fc253cfa69fcf4ec77f
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-06 15:32:42 +0100

    improves indentation for drinks stock dictionaries

--- src/drinks_stock.py
+++ src/drinks_stock.py
@@ -1,20 +1,23 @@
 drinks_stock = [
-    {"name": "Beavertown",
-     "type": "Beer",
-     "price": 5,
-     "alcohol_level": 0.05,
-     "stock_levels": 10
+    {
+        "name": "Beavertown",
+        "type": "Beer",
+        "price": 5,
+        "alcohol_level": 0.05,
+        "stock_levels": 10
     },
-    {"name": "Mojito",
-     "type": "Cocktail",
-     "price": 12,
-     "alcohol_level": 0.10,
-     "stock_levels": 40
+    {
+        "name": "Mojito",
+        "type": "Cocktail",
+        "price": 12,
+        "alcohol_level": 0.10,
+        "stock_levels": 40
     },
-    {"name": "Rum and Coke",
-     "type": "Cocktail",
-     "price": 10,
-     "alcohol_level": 0.10,
-    "stock_levels": 10
+    {
+        "name": "Rum and Coke",
+        "type": "Cocktail",
+        "price": 10,
+        "alcohol_level": 0.10,
+        "stock_levels": 10
     },
 ]
