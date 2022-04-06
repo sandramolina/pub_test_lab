bbdd8230f67694c6d8a481d43ecd05a8e8c394d7
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-06 11:15:16 +0100

    defines Drink class

--- classes/drink.py
+++ classes/drink.py
@@ -1,2 +1,5 @@
 class Drink:
-    
+    def __init__(self, input_brand_name, input_type, input_price):
+        self.brand_name = input_brand_name
+        self.type = input_type
+        self.price = input_price
--- tests/test_pub.py
+++ tests/test_pub.py
@@ -5,7 +5,14 @@ from classes.pub import Pub
 class TestPub(unittest.TestCase):
     
     def setUp(self):
+        
         self.pub_1 = Pub("SanDraagon", ["Ale"])
 
     def test_pub_name(self):
         self.assertEqual("SanDraagon", self.pub_1.name)
+
+    def test_pub_till(self):
+        self.assertEqual(100, self.pub_1.till)
+
+    def test_pub_drinks(self):
+        self.assertEqual("Ale". self.pub_1.drinks)
