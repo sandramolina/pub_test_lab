1d1ac50d2e9e057cf4fa7b206a8f4108339bf32a
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-06 14:22:07 +0100

    adds food class and test files

---
+++ classes/food.py
@@ -0,0 +1,6 @@
+class Food:
+    def __init__(self, input_name, input_price, input_rejuvenation_level):
+        self.food_name = input_name
+        self.food_price = input_price
+        self.rejuvenation_level = input_rejuvenation_level
+        
